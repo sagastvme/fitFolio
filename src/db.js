6de54a498315be7ea446d91c5db6f528b8689656
Eduardo <miltormentas@tutanota.com>
2023-10-02 18:08:56 -0700

    created app with bun and elysia

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const uri = "mongodb://localhost:27017/gym";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+    });
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Elysia } from 'elysia'
+import { html } from '@elysiajs/html'
+import { mongoose } from "@lucia-auth/adapter-mongoose";
+new Elysia()
+    .use(html())
+    .get(
+        '/',
+        () => console.log(mongoose)
+    )
+    .get('/jsx', () => (`
+        <html lang="en">
+            <head>
+                <title>Hello World</title>
+            </head>
+            <body>
+                <h1>Hello World</h1>
+            </body>
+        </html>`
+    ))
+    .listen(8080)
