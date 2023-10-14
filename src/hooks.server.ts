c0839d612c74d829182b66b65e461a0a711de76f
Eduardo <miltormentas@tutanota.com>
2023-10-14 10:24:23 -0400

    removed boilerplate

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-// src/hooks.server.ts
-import { auth } from "$lib/server/lucia";
-import type { Handle } from "@sveltejs/kit";
-
-export const handle: Handle = async ({ event, resolve }) => {
-	// we can pass `event` because we used the SvelteKit middleware
-	event.locals.auth = auth.handleRequest(event);
-	return await resolve(event);
-};
