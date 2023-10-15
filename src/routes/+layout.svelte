8dd70982a6d139fc61e50164399814a91eb7a89d
Eduardo <miltormentas@tutanota.com>
2023-10-15 12:46:59 -0400

    installed tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
-<nav>
-    {#each data.sections as section (section.slug)}
-      <a href={section.slug}>{section.title}</a>
-    {/each}
+  <nav class="bg-gradient-to-r from-black to-red-950">
+    <a href="">
+
+      <img src="/favicon.png" class="rounded-full h-12 md:h-20 ml-5" alt="Fit Folio logo">
+    </a>
   </nav>
   
   <slot></slot>
   
   <script>
     /** @type {import('./$types').PageData} */
+  import "../app.css";
+  import { page } from '$app/stores';
     export let data;
   </script>
   <style>
@@ -16,3 +19,4 @@
         margin:4px
     }
   </style>
+
