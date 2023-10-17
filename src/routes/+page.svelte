c0ea56a05308415b7c3c32d793adc6fb68880c94
Eduardo <miltormentas@tutanota.com>
2023-10-16 22:04:55 -0400

    created welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <script>
-
-</script>
-
-<svelte:head>
+	
+  </script>
+  
+  <svelte:head>
 	<title>Fit Folio</title>
-</svelte:head>
+  </svelte:head>
+  
+  <div class="mt-24 md:mt-44 w-screen flex items-center justify-center">
+	<div class="text-center flex flex-col items-center justify-center">
+	  <h1 class="text-5xl font-bold text-white">Do you have what it takes?</h1>
+	  <p class="text-lg text-gray-300 mt-10 w-1/2 ">"If you are afraid to die, embrace the journey of life with unwavering courage, for the very act of being born is an epic adventure, and within it, lies the true essence of fearlessness."</p>
+	 <div class="flex flex-row gap-8">
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	  <a href="login" class="bg-red-600 text-white font-semibold py-2 px-4 mt-8 rounded-lg hover:bg-red-700">Log in</a>
+	  <a href="signup" class="bg-red-600 text-white font-semibold py-2 px-4 mt-8 rounded-lg hover:bg-red-700">Sign up</a>
+	</div> 
+
+	</div>
+  </div>
+  
