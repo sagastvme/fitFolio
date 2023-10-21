206c0a6feaac30c36982100c6af756cca78572f1
Eduardo <miltormentas@tutanota.com>
2023-10-21 14:13:15 -0400

    updated login and userSchema

--- src/db/schema/userSchema.ts
+++ src/db/schema/userSchema.ts
@@ -1,14 +1,41 @@
-class UserSchema{
+class UserSchema {
     email: string;
     password: string;
     _id: string;
     preferred_units: string;
-    constructor(email: string, password: string, _id:string, preferred_units: string){
-        this.email= email,
-        this.password= password,
-        this._id= _id,
-        this.preferred_units = preferred_units
+    created_at: string;
+    initial_weight: string;
+    height: string;
+    routine: object; // Specify the type as 'object'
+    objective: string;
+    routine_updated_at: string;
+    updated_weight: string;
+
+    constructor(
+        email: string,
+        password: string,
+        _id: string,
+        preferred_units: string,
+        created_at: string,
+        initial_weight: string,
+        height: string,
+        routine: object, // Specify the type as 'object'
+        objective: string,
+        routine_updated_at: string,
+        updated_weight: string
+    ) {
+        this.email = email;
+        this.password = password;
+        this._id = _id;
+        this.preferred_units = preferred_units;
+        this.created_at = created_at;
+        this.initial_weight = initial_weight;
+        this.height = height;
+        this.routine = routine;
+        this.objective = objective;
+        this.routine_updated_at = routine_updated_at;
+        this.updated_weight = updated_weight;
     }
 }
 
-export default UserSchema
+export default UserSchema;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,34 @@
 <script>
-	
+	  import { onMount, afterUpdate } from 'svelte';
+	  let isLoggedIn = false;
+	  let email =''
+	 let nameWithoutDomain = ''; 
+	 let randomIcon = ''
+	  onMount(() => {
+  if (typeof window !== 'undefined') {
+    try {
+      if(localStorage.getItem('loggedIn')){
+      const storedData = JSON.parse(localStorage.getItem('loggedIn'));
+      isLoggedIn= storedData.loggedIn;
+	  email = storedData.email;
+		nameWithoutDomain = email.substring(0, email.indexOf('@'));
+		nameWithoutDomain = nameWithoutDomain[0].toUpperCase() + nameWithoutDomain.substring(1)
+		let randomNumber = Math.floor(Math.random() * 9);
+
+		randomIcon = '/icons/icon_'+randomNumber+'.svg'
+	}
+   } catch (error) {
+      console.error('Error parsing data from localStorage:', error);
+      isLoggedIn = false; // Set a default value if parsing fails.
+    }
+  }
+});
   </script>
   
   <svelte:head>
 	<title>Fit Folio</title>
   </svelte:head>
-  
+  {#if !isLoggedIn}
   <div class="mt-24 md:mt-44 w-screen flex items-center justify-center">
 	<div class="text-center flex flex-col items-center justify-center">
 	  <h1 class="text-5xl font-bold text-white">Do you have what it takes?</h1>
@@ -18,4 +41,24 @@
 
 	</div>
   </div>
+  {:else}
+  <div class=" text-white gap-4 flex flex-col items-center justify-center">
+	<h2>Welcome to your portal {nameWithoutDomain}</h2>
+	<div class="bg-red-700 rounded-full">
+		<img src={randomIcon} class="h-32 p-4" alt="icon">
+	</div>
+	<div class="flex items-center justify-center flex-col">
+		<h3>Your stats</h3>
+		<p>Your objective as of right now: Lose weight</p>
+		<p>Your height: 184cm</p>
+		<p>Your initial weight</p>
+		<p>Your weight: 200lbs</p>
+		<p>Days since you started this journey: 84 days</p>
+		<p>Weight progression: +8kg</p>
+		<p>Your BMI: 8 points?</p>
+		<p>Your recommended BMI: 20 points</p>
+		<p>Your exercise for today is : Upper body</p>
+	</div>
+  </div>
+  {/if}
   
