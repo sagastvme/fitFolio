3b23a65a6b3243463040904950a87760c6260b90
eduardo <miltormentas@tutanota.com>
2023-11-10 23:10:26 -0500

    improved dictionary

---
+++ lang/fr/welcome.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+
+    'header' => 'Are you ready to start your journey in Fit Folio ?',
+    'body' => 'Fit Folio is an app that lets you track your gym progress to see how well you are doing when exercising',
+    'footer' => 'This is the footer lol',
+];
