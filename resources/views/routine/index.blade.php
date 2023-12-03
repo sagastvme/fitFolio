dd4722cefe16293c7cff511031299742018769bb
eduardo <miltormentas@tutanota.com>
2023-12-02 21:01:17 -0500

    better ui

--- resources/views/routine/index.blade.php
+++ resources/views/routine/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-
-
 @section('content')
 
     <div class="container">
@@ -9,44 +7,51 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        Your routine summary
+                        Your Routine Summary
                     </div>
                     <div class="card-body">
 
                         @error('duration')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                        <div class="alert alert-danger" role="alert">
+                            {{ $message }}
+                        </div>
                         @enderror
                         @error('name')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{$message }}</strong>
-                                    </span>
+                        <div class="alert alert-danger" role="alert">
+                            {{ $message }}
+                        </div>
                         @enderror
                         @error('day')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                        <div class="alert alert-danger" role="alert">
+                            {{ $message }}
+                        </div>
                         @enderror
-                       <p>You have {{sizeof($workouts)}} workouts</p>
-                        <p>Your routine is {{$totalUniqueDaysCount}} days long</p>
 
+                        <p class="lead">You have {{ sizeof($workouts) }} workouts</p>
+                        <p class="lead">You workout {{ $totalUniqueDaysCount }} days</p>
+                        <p class="lead">You rest {{ 7 - $totalUniqueDaysCount }} days</p>
 
                         @foreach($daysOfWeekOrder as $day => $order)
-                            <p>Workout for {{ $day }}</p>
-                            @php
-                                $workoutsForDay = $workouts->filter(function ($workout) use ($day) {
-                                    return $workout['day'] === $day;
-                                });
-                            @endphp
+                            <div class="mt-3">
+                                <h5 class="bold-text">{{ $day }}</h5>
+                                @php
+                                    $workoutsForDay = $workouts->filter(function ($workout) use ($day) {
+                                        return $workout['day'] === $day;
+                                    });
+                                @endphp
 
-                            @if($workoutsForDay->isEmpty())
-                                <p class="accordion-button">Rest day</p>
-                            @else
-                                @foreach($workoutsForDay as $w)
-                                    <a href="{{ route('workout.show', ['id' => $w->getId()]) }}">{{ $w->getName() }}</a>
-                                @endforeach
-                            @endif
+                                @if($workoutsForDay->isEmpty())
+                                    <p class="text-muted">Rest day</p>
+                                @else
+                                    <ul class="list-group">
+                                        @foreach($workoutsForDay as $w)
+                                            <li class="list-group-item">
+                                                <a href="{{ route('workout.show', ['id' => $w->getId()]) }}">{{ $w->getName() }}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
+                            </div>
                         @endforeach
                     </div>
                 </div>
